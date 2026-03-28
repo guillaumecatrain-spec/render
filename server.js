55031ad534b1bfe07dcc542ee33bb0c47469100a
guillaumecatrain-spec <guillaume.catrain@gmail.com>
2026-03-28 11:42:58 +0100

    Update display route to use new HTML file

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.get('/tides', async (req, res) => {
 
 // ✅ Page d'affichage Frame 32
 app.get('/display', (req, res) => {
-  res.sendFile('tides_display_frame32_centered.html', { root: '.' });
+  res.sendFile('tides_display_frame32_centered_4tides.html', { root: '.' });
 });
 
 app.get('/', (req, res) => {
